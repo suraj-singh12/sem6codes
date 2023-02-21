1580fb90ea447077c70923051887ef25f5ce69e0
suraj-singh12 <surajsingh5092@gmail.com>
2023-02-21 10:48:52 +0530

    updated with react info

--- web-dev-with-react/react/classNotes.js
+++ web-dev-with-react/react/classNotes.js
@@ -1,3 +1,29 @@
+// React enables us to make SPA
+/** SPA (Single Page Applications)
+ * A single page application is a single page design for a web page which loads for once and change the content on the page by fetching data several time without loading the page.
+ * Popular framework for attaining SPA - ReactJS, AngularJS, etc.
 
-// SPA (Single Page Applications)
+ ** What is react?
+ * Simple, feature rich, component based javaScript UI library.
+ * provides state management, routing, etc on top of react library (so react is a framework also)
 
+ ** Advantages of react
+ * Better Caching
+ * Less bandwidth consumption
+ * Better user experience
+ 
+ ** Features of React
+ * JSX: allows to write html in js file itself
+ * Components: Break entire website into components
+ * Virtual DOM (Virtual Document Object Model)
+ *              ReactJS creates a copy of the actual dom, and on any user action it makes changes in virtual dom only, then only the components that have truely changed are updated on the actual DOM, hence improving the performance of the website (by not refreshing and asking all the files from server again and again on every user action as is in non-react simple html js websites.)
+ * Reusable components
+ * Benefit of having a js library
+ 
+
+ ** Disadvantages
+ * High pace of development of react: Need to update the webapps as the react gets upgraded, so as to avoid using deprecated things.
+ * Poor Documentation
+ * View Part: It's only the frontEnd.
+ * 
+ */
