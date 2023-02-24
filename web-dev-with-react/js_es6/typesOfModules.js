baf1331b4c11933584a7f0961d9d2c7ee99bc1fd
suraj-singh12 <surajsingh5092@gmail.com>
2023-02-24 10:25:09 +0530

    added fs and typesOfModules

--- web-dev-with-react/js_es6/typesOfModules.js
+++ web-dev-with-react/js_es6/typesOfModules.js
@@ -14,26 +14,37 @@
 
 
 const fs = require('fs');       // is an asynchronous fn
-
 let fileName = 'myFile.txt';
 let data = 'this is a file';
 
+// ---------------------------------------------------
 // fs.writeFile(fileName, data, (err)=> {
 //     if(err) console.log("An error occurred!\n");
 //     else console.log('data written')
 // });
 
-fs.readFile(fileName, (err, data) => {
-    if(err) console.log('an error occurred!\n');
-    else {
-        console.log(data, '\n');      // will give op in buffer form
-        console.log(data.toString());       // convert to type String
-    }
-});
-console.log('myProgram ends here');
+
+// write synchronously
+// try {
+//     fs.writeFileSync(fileName, data + ': sync manner');
+//     console.log('data written synchronously!');
+// } catch (err) {
+//     console.log('error occurred while writing to file')
+// }
+// ---------------------------------------------------
+
+// Program 1
+// fs.readFile(fileName, (err, data) => {
+//     if(err) console.log('an error occurred!\n');
+//     else {
+//         console.log(data, '\n');      // will give op in buffer form
+//         console.log(data.toString());       // convert to type String
+//     }
+// });
+// console.log('myProgram ends here');
 
 /**
- * Output: 
+ * Program 1 Output: 
 
 myProgram ends here
 <Buffer 74 68 69 73 20 69 73 20 61 20 66 69 6c 65 2e 20 41 6e 64 20 69 20 61 6d 20 61 70 
@@ -41,7 +52,37 @@ myProgram ends here
 
 this is a file. And i am appending more data to it.
 
-why does it happen so?
+
+> why does it happen so?
 Because nodejs is asynchronous, it does not wait for anything.
 
+Nodejs is single threaded. Check how this works! 1
+Every request will have one unique request, and it will complete that only.
 */
+
+// Program 2: reading synchronously
+// let fileData;
+// try {
+//     fileData = fs.readFileSync(fileName);
+//     console.log(fileData, '\n');
+//     console.log(fileData.toString());
+// } catch(err) {
+//     console.log('Error occurred while reading the file!!')
+// }
+// console.log('myProgram ends here')
+
+
+
+// ------------- Experimenting ---------------------
+fs.writeFile(fileName, data, (err) => {
+    if(err) console.log('an error occurred');
+    else {
+        try {
+            let data = fs.readFileSync(fileName)
+            console.log(data.toString());
+        } catch(err) {
+            console.log('error in reading data');
+        }
+        console.log('data was written');
+    }
+})
