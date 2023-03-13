e52e87410056f6d967a8590fbea6c8e12700493b
suraj-singh12 <surajsingh5092@gmail.com>
2023-03-13 11:42:37 +0530

    updated express

--- web-dev-with-react/js_es6 INT222/express/form.js
+++ web-dev-with-react/js_es6 INT222/express/form.js
@@ -16,13 +16,40 @@ app.get('/apply', (req, res)=> {
     // console.log(req);
     // console.log(req.query);
     // console.log(response);
+    
+    let data = response;
+    let result = (`
+    <table border="1px" style="padding: 5px">
+    <thead>
+        <tr>
+            <th style="padding: 1rem">First Name</th>
+            <th style="padding: 1rem">Last Name</th>
+            <th style="padding: 1rem">Gender</th>
+            <th style="padding: 1rem">Address</th>
+        </tr> 
+    </thead> 
+    <tbody> 
+        <tr>
+            <td style="padding: 1rem">${data.firstName}</td>
+            <td style="padding: 1rem">${data.lastName}</td>
+            <td style="padding: 1rem">${data.gender}</td>
+            <td style="padding: 1rem">${data.address}</td>
+        </tr>
+    </tbody>
+    </table>
+    `);
     fs.appendFile('logFile.log', `,\n${JSON.stringify(response)}`, (err, data) => {
         if (err) throw err;
         console.log('Data written to file');
         });
-    res.send(response);
+    res.send(result)
+    // res.send(response);
 });
 
 app.listen(8000, () => {
     console.log('Server is running at : http://localhost:8000');
 });
+
+
+// try these changes: 
+// use post in html, then app.post in js, then use body-parser, then use req.body instead of req.query
