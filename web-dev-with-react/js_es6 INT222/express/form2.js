a29e80f9433ff48fb56186160e8d5c9a985f1c41
suraj-singh12 <surajsingh5092@gmail.com>
2023-03-13 11:47:09 +0530

    using bodyParser, and post method to receive data and send

---
+++ web-dev-with-react/js_es6 INT222/express/form2.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser  = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/' + 'form2.html');
+})
+
+app.post('/apply', (req, res) => {
+    response = {
+        firstName:req.body.first_name,
+        lastName:req.body.last_name,
+        gender:req.body.gender,
+        address:req.body.address
+    }
+    console.log(response)
+    res.send(response);
+})
+
+app.listen(8900, ()=> {
+    console.log('server running at : http://localhost:8900/')
+})
