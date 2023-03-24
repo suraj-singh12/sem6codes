cb85876bcdda68dd3f1d2a752c778a23ccdfa46a
suraj-singh12 <surajsingh5092@gmail.com>
2023-03-24 09:51:03 +0530

    added cookies

---
+++ web-dev-with-react/js_es6 INT222/cookie/cookieDemo.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());            // middleware cookieParser, will be called everytime a request is made to any route before the route is called
+
+// set the cookie
+app.get('/', (req, res) => {
+    let userData = {
+        name: 'John',
+        age: 30,
+        profile: 'admin'
+    };
+    // res.cookie('course', 'html').send('Our website has set the cookies');
+    res.cookie('user', userData).send('<h1>Our website has set the NEW cookies</h1>');
+    console.log('Cookies: ', req.cookies);
+});
+
+// clear the cookie
+app.get('/clear', (req, res) => {
+    res.clearCookie('course', 'html');
+    res.send('<h1>Cookies cleared</h1>');
+});
+
+app.listen(3200, () => {
+    console.log('Server is running on port 3200');
+    console.log('http://localhost:3200');
+})
